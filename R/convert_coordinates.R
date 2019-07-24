5e379c0a7b89dfa23baf7c042552ec08d9077597
Nicole Kramer <nekramer@live.unc.edu>
2019-07-24 14:17:18 -0400

    name change from gridplotHic to bb_hic
    Former-commit-id: f99aa7c638f57076d2bd52f717103a5b29de74e6

--- R/convert_coordinates.R
+++ R/convert_coordinates.R
@@ -1,14 +1,14 @@
-convert_coordinates <- function(height, width, x, y, pageheight=11){
-  
+convert_coordinates <- function(height, width, x, y, pageheight){
+
   #standardized to 8 1/2 by 11 inch piece of paper
   #given x and y coordinates in the top left
-  #y coordinate based as if going from top of page
-  
+  #y coordinate based as if going from  of page
+
   ybottom = pageheight - y
-  
+
   x1 = x + (0.5*width)
   y1 = ybottom - (0.5*height)
-  
-  return(list(x1,y1))
-  
+
+  return(list(x1, y1))
+
 }
