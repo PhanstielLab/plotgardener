01efbc5a4d40200af96e3d68fca77e3b272082c7
Nicole Kramer <nekramer@live.unc.edu>
2019-08-19 14:45:28 -0400

    updates to bb_plotgenes

--- R/bb_plotgenes.R
+++ R/bb_plotgenes.R
@@ -1,7 +1,8 @@
 #' @export
 #'
 bb_plotgenes <- function(gtf, chrom = "chr8", chromstart = 133600000, chromend = 134800000, strand = "+", col = "black",
-                         height = 1, width = 3, x = 1, y = 1, fill = NA, gene_height = 0.5){
+                         height = 1, width = 3, x = 1, y = 1, fill = NA, gene_height = 0.5, label = TRUE,
+                         fontsize = 5, fontfamily = "", fontface = "plain"){
 
   gtf <- rtracklayer::import(gtf)
   gtf_df <- as.data.frame(gtf)
@@ -12,10 +13,10 @@ bb_plotgenes <- function(gtf, chrom = "chr8", chromstart = 133600000, chromend =
   ## Get data in correct format
   gtf_subset <- data.frame(gtf_subset$start, gtf_subset$end, gtf_subset$gene_name)
 
-  ## Viewport navigation
-  if(is.null(current.vpPath()) == FALSE){
-    upViewport()
-  }
+  # ## Viewport navigation
+  # if(is.null(current.vpPath()) == FALSE){
+  #   upViewport()
+  # }
 
   ## Get page_height and margins from bbEnv
   page_height <- get("page_height", envir = bbEnv)
@@ -30,26 +31,32 @@ bb_plotgenes <- function(gtf, chrom = "chr8", chromstart = 133600000, chromend =
   grid.segments(x0 = unit(0, "npc"), y0 = unit(0.5, "npc"), x1 = unit(1, "npc"), y1 = unit(0.5, "npc"))
 
   ## Function to plot boxes where exons are
-  draw_exon <- function(df, chromstart, chromend, col, fill, gene_height){
+  draw_exon <- function(df, chromstart, chromend, col, fill, gene_height, label, fontsize, fontfamily, fontface){
 
-    gene_name <- df[3]
+
     start <- as.numeric(df[1])
     start.normalized <- normalize(start, chromstart, chromend)
     stop <- as.numeric(df[2])
     stop.normalized <- normalize(stop, chromstart, chromend)
     ybottom <- 0.5 - (0.5 * gene_height)
     ytop <- 0.5 + (0.5 * gene_height)
-    name_location <- mean(c(start.normalized, stop.normalized))
+
 
     grid.polygon(x = c(start.normalized, start.normalized, stop.normalized, stop.normalized), y = c(ybottom, ytop, ytop, ybottom),
                  gp = gpar(col = col, fill = fill))
 
     ## Gene label
-    grid.text(gene_name, x = unit(name_location, "npc"), y = unit(0.5, "npc"))
+    if (label == TRUE){
+      gene_name <- df[3]
+      name_location <- mean(c(start.normalized, stop.normalized))
+      grid.text(gene_name, x = unit(name_location, "npc"), y = unit(0.5, "npc"))
+    }
+
 
   }
 
-  invisible(apply(gtf_subset, 1, draw_exon, chromstart = chromstart, chromend = chromend, col = col, fill = fill, gene_height = gene_height))
+  invisible(apply(gtf_subset, 1, draw_exon, chromstart = chromstart, chromend = chromend, col = col, fill = fill,
+                  gene_height = gene_height, label = label, fontsize = fontsize, fontfamily = fontfamily, fontface = fontface))
 
 
 }
