8fe386cabf482e4b726da6a0a6431fdacb850913
Nicole Kramer <nekramer@live.unc.edu>
2019-08-18 23:07:02 -0400

    updates to bb_plotgenes

--- R/bb_plotgenes.R
+++ R/bb_plotgenes.R
@@ -1,12 +1,16 @@
 #' @export
 #'
-bb_plotgenes <- function(gtf, chrom = "chr8", chromstart = 133600000, chromend = 134800000, strand = "+", col = "black", height, width, x, y){
+bb_plotgenes <- function(gtf, chrom = "chr8", chromstart = 133600000, chromend = 134800000, strand = "+", col = "black",
+                         height = 1, width = 3, x, y, fill = NA, gene_height = 0.5){
 
   gtf <- rtracklayer::import(gtf)
   gtf_df <- as.data.frame(gtf)
 
   ## Subset for exons in region
   gtf_subset <- gtf_df[which(gtf_df$seqnames == chrom & gtf_df$start >= chromstart & gtf_df$end <= chromend & gtf_df$type == "exon" & gtf_df$strand == strand),]
+
+  ## Get data in correct format
+  gtf_subset <- c(gtf_subset$start, gtf_subset$stop, gtf_subset$gene_name)
 
   ## Viewport navigation
   if(is.null(current.vpPath()) == FALSE){
@@ -26,26 +30,26 @@ bb_plotgenes <- function(gtf, chrom = "chr8", chromstart = 133600000, chromend =
   grid.segments(x0 = unit(0, "npc"), y0 = unit(0.5, "npc"), x1 = unit(1, "npc"), y1 = unit(0.5, "npc"))
 
   ## Function to plot boxes where exons are
-  draw_exon <- function(df, chromstart, chromend){
+  draw_exon <- function(df, chromstart, chromend, col, fill, gene_height){
 
-    gene_name <- df[14]
-    start <- as.numeric(df[2])
+    gene_name <- df[3]
+    start <- as.numeric(df[1])
     start.normalized <- normalize(start, chromstart, chromend)
-    stop <- as.numeric(df[3])
+    stop <- as.numeric(df[2])
     stop.normalized <- normalize(stop, chromstart, chromend)
-    ybottom <- 0.25
-    ytop <- 0.75
+    ybottom <- 0.5 - (0.5 * gene_height)
+    ytop <- 0.5 + (0.5 * gene_height)
     name_location <- mean(c(start.normalized, stop.normalized))
 
     grid.polygon(x = c(start.normalized, start.normalized, stop.normalized, stop.normalized), y = c(ybottom, ytop, ytop, ybottom),
-                 gp = gpar(col = "black"))
+                 gp = gpar(col = col, fill = fill))
+
+    ## Gene label
     grid.text(gene_name, x = unit(name_location, "npc"), y = unit(0.5, "npc"))
 
   }
 
-  invisible(apply(gtf_subset, 1, draw_exon, chromstart = chromstart, chromend = chromend))
-
-  ## Label
+  invisible(apply(gtf_subset, 1, draw_exon, chromstart = chromstart, chromend = chromend, col = col, fill = fill, gene_height = gene_height))
 
 
 }
