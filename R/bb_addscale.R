c681b985fab7429d9259cbb35817d094ed16aa8b
Nicole Kramer <nekramer@live.unc.edu>
2019-11-22 11:29:51 -0500

    added ability for scale plot to be removed

--- R/bb_addscale.R
+++ R/bb_addscale.R
@@ -29,14 +29,14 @@ bb_addscale <- function(color_vector, min_val, max_val, border = FALSE, height, 
   errorcheck_bb_addscale <- function(bb_scale){
 
     ## checking min_val and max val
-    if (as.numeric(bb_legend$min_val) > as.numeric(bb_legend$max_val)){
+    if (as.numeric(bb_scale$min_val) > as.numeric(bb_scale$max_val)){
 
       warning("/'min_val/' is larger than /'max_val/'. Scale labels may be incorrect.")
 
     }
 
     ## check proper orientation
-    if (!bb_legend$orientation %in% c("vertical", "horizontal")){
+    if (!bb_scale$orientation %in% c("vertical", "horizontal")){
 
       stop("Invalid /'orientation/' parameter. Options are 'vertical' or 'horizontal'.")
 
@@ -49,13 +49,13 @@ bb_addscale <- function(color_vector, min_val, max_val, border = FALSE, height, 
   # ======================================================================================================================================================================================
 
   bb_scale <- structure(list(color_palette = color_vector, min_val = min_val, max_val = max_val,
-                              orientation = orientation, height = height, width = width, x = x, y = y, units = units), class = "bb_scale")
+                              orientation = orientation, height = height, width = width, x = x, y = y, units = units, grobs = NULL), class = "bb_scale")
 
   # ======================================================================================================================================================================================
   # CALL ERRORS
   # ======================================================================================================================================================================================
 
-  errorcheck_bb_addlegend(bb_scale = bb_scale)
+  errorcheck_bb_addscale(bb_scale = bb_scale)
 
   # ======================================================================================================================================================================================
   # VIEWPORTS
@@ -69,6 +69,12 @@ bb_addscale <- function(color_vector, min_val, max_val, border = FALSE, height, 
                  x = unit(page_coords[[1]]$x, page_coords[[3]]), y = unit((page_coords[[2]]-page_coords[[1]]$y), page_coords[[3]]), just = just, name = "bb_scale")
 
   pushViewport(vp)
+
+  # ======================================================================================================================================================================================
+  # GROBS
+  # ======================================================================================================================================================================================
+
+  assign("scale_grobs", gTree(name = "scale_grobs"), envir = bbEnv)
 
   # ======================================================================================================================================================================================
   # VERTICAL ORIENTATION
@@ -88,15 +94,22 @@ bb_addscale <- function(color_vector, min_val, max_val, border = FALSE, height, 
 
     new_height <- 1 - (lH + hH + dH)
 
+    color_scale <- rasterGrob(rev(color_vector), width = unit(page_coords[[1]]$width, page_coords[[3]]), height = unit(new_height, "npc"), y = 1 - (hH + (0.5 * dH)), just = "top")
+
     grid.draw(lowLab)
     grid.draw(highLab)
+    grid.draw(color_scale)
 
-    grid.raster(rev(color_vector), width = unit(page_coords[[1]]$width, page_coords[[3]]), height = unit(new_height, "npc"), y = 1 - (hH + (0.5 * dH)), just = "top")
+    assign("scale_grobs", addGrob(gTree = get("scale_grobs", envir = bbEnv), child = lowLab), envir = bbEnv)
+    assign("scale_grobs", addGrob(gTree = get("scale_grobs", envir = bbEnv), child = highLab), envir = bbEnv)
+    assign("scale_grobs", addGrob(gTree = get("scale_grobs", envir = bbEnv), child = color_scale), envir = bbEnv)
 
     if (border == T){
 
-      grid.rect(y = 1 - (hH + (0.5 * dH)), just = "top", width = unit(page_coords[[1]]$width, page_coords[[3]]), height = unit(new_height, "npc"),
+      borderGrob <- rectGrob(y = 1 - (hH + (0.5 * dH)), just = "top", width = unit(page_coords[[1]]$width, page_coords[[3]]), height = unit(new_height, "npc"),
                 gp = gpar(col = "black", fill = NA) )
+      grid.draw(borderGrob)
+      assign("scale_grobs", addGrob(gTree = get("scale_grobs", envir = bbEnv), child = borderGrob), envir = bbEnv)
 
     }
 
@@ -120,20 +133,30 @@ bb_addscale <- function(color_vector, min_val, max_val, border = FALSE, height, 
 
     new_width <- 1 - (hW + lW + dW)
 
+    color_scale <- rasterGrob(matrix(data = color_vector, nrow = 1, ncol = length(color_vector)), width = unit(new_width, "npc"), height = unit(page_coords[[1]]$height, page_coords[[3]]),
+                x = lW + (0.5 * dW), just = "left")
+
     grid.draw(lowLab)
     grid.draw(highLab)
+    grid.draw(color_scale)
 
-    grid.raster(matrix(data = color_vector, nrow = 1, ncol = length(color_vector)), width = unit(new_width, "npc"), height = unit(page_coords[[1]]$height, page_coords[[3]]),
-                x = lW + (0.5 * dW), just = "left")
+    assign("scale_grobs", addGrob(gTree = get("scale_grobs", envir = bbEnv), child = lowLab), envir = bbEnv)
+    assign("scale_grobs", addGrob(gTree = get("scale_grobs", envir = bbEnv), child = highLab), envir = bbEnv)
+    assign("scale_grobs", addGrob(gTree = get("scale_grobs", envir = bbEnv), child = color_scale), envir = bbEnv)
 
     if (border == T){
 
-      grid.rect(x = lW + (0.5 * dW), just = "left", width = unit(new_width, "npc"), height = unit(page_coords[[1]]$height, page_coords[[3]]),
+      borderGrob <- rectGrob(x = lW + (0.5 * dW), just = "left", width = unit(new_width, "npc"), height = unit(page_coords[[1]]$height, page_coords[[3]]),
                 gp = gpar(col = "black", fill = NA) )
+      grid.draw(borderGrob)
+      assign("scale_grobs", addGrob(gTree = get("scale_grobs", envir = bbEnv), child = borderGrob), envir = bbEnv)
 
     }
 
   }
+
+  grob_list <- lapply(get("scale_grobs", envir = bbEnv)$children, convert_gpath)
+  bb_scale$grobs = grob_list
 
   ## Go back to root viewport
   upViewport()
