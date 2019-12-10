2b5d3c8a16b2a81ab50788e7ec717ef7b744498d
Nicole Kramer <nekramer@live.unc.edu>
2019-12-10 14:33:56 -0500

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit b1b2e2788246d68ce89480e5a751bf108659ff06 [formerly 563d8f4e8311606116ef5e17d8a9798462babdfd].
    
    
    Former-commit-id: 596fa8994217559212fdb1f55a516b6da9b65697

--- R/appendGuide.R
+++
@@ -1,5 +0,0 @@
-appendGuide <- function(x){
-  #envir$guides[[length(envir$guides)+1]] <- x
-  assign("guides", addGrob(gTree = get("guides", envir = bbEnv), child = x), envir = bbEnv)
-  grid.draw(x)
-}
