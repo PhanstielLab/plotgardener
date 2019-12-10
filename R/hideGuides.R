d033f921c466bfe2d7f83d99afd3a7a76653082f
Nicole Kramer <nekramer@live.unc.edu>
2019-12-10 14:33:17 -0500

    updates
    Former-commit-id: 16a99d110524a6b55b9ea70321273cbbe4cf4f70 [formerly 09c5172adcfe50ce866961221e4b19c3d136497e]
    Former-commit-id: 6faa0e29e5b13eaf3cef5202891ff9bc59063652

--- R/hideGuides.R
+++
@@ -1,6 +0,0 @@
-hideGuides <- function(envir=bbEnv){
-  if(length(envir$guides) > 0){
-    for(i in 1:length(envir$guides)) grid.remove(gPath(envir$guides[[i]]$name)$name)
-    bbEnv$guides <- list()
-  }
-}
