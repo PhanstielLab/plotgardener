dc846d4a65656f59fc96067c486800fd2fa999f5
Nicole Kramer <nekramer@live.unc.edu>
2019-12-12 11:40:47 -0500

    temporary fix to guides error calls
    Former-commit-id: 89a68afa762391d744e8eec5da462f5caa058f99

--- R/bb_vguide.R
+++ R/bb_vguide.R
@@ -8,13 +8,13 @@
 bb_vGuide <- function(x, units = "inches", col = "grey55"){
 
   ## Get the names of the current viewports
-  current_viewports <- lapply(current.vpTree()$children, viewport_name)
-
-  if (!"bb_page" %in% current_viewports){
-
-    stop("Must make a BentoBox page with bb_makePage() before adding additional guidelines.")
-
-  }
+  # current_viewports <- lapply(current.vpTree()$children, viewport_name)
+  #
+  # if (!"bb_page" %in% current_viewports){
+  #
+  #   stop("Must make a BentoBox page with bb_makePage() before adding additional guidelines.")
+  #
+  # }
 
   guide <- grid.segments(x0 = x, x1 = x, y0 = unit(0, units = "npc"), y1 = unit(1, units = "npc"), default.units = units,
                            gp = gpar(col = col))
