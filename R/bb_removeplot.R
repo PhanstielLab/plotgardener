c2bd9063864604ddd9b92cca383f24755b3ba6ad
Nicole Kramer <nekramer@live.unc.edu>
2020-06-08 11:50:09 -0400

    Nucleotide labeling to bb_labelGenome
    also includes some edits to roxygen2 comments in this and related functions
    
    Former-commit-id: 13c435f8b14c43386ffd1e5fe6ea3aa5b7a87e1b [formerly 13c435f8b14c43386ffd1e5fe6ea3aa5b7a87e1b [formerly eb0b40e4a2e83a5504dba0f37cbda10bc867bd79 [formerly eb0b40e4a2e83a5504dba0f37cbda10bc867bd79 [formerly 61de4be65eeee376f2f0f0006564a3e15e3fc44d [formerly 4cc821ca610f240985757c95233d39986a355d9f]]]]]
    Former-commit-id: cced77d80f28cd2be142bc2f19ac88ea700106d6
    Former-commit-id: 6cbd81d36418fae59e27cf13144635dc614034f6
    Former-commit-id: b9487250413d68048d9e666aceba48a4f7dfe5c7
    Former-commit-id: 03e1ffbf3c35a40418f646d4f137570292b43061
    Former-commit-id: afd33d65f3628b057bebbdf0ef357ba99556fbf9
    Former-commit-id: 29c4a63219e207cfe4a63fbc7112f9febe0cdd35

--- R/bb_removeplot.R
+++ R/bb_removeplot.R
@@ -1,34 +1,12 @@
-#' removes BentoBox plots
+#' Removes BentoBox plots and annotations
 #'
-#' @param plot BentoBox plot to remove; defined by the output of a BentoBox plotting function
+#' @param plot BentoBox plot to remove, defined by the output of a BentoBox plotting function.
 #'
 #' @export
-#'
 bb_removePlot <- function(plot){
 
   ## error function: need a plot that's a valid BentoBox plot, make sure the plot is plotted
-
-  ## get the grobs of the plot and convert to gpath
-  #grob_list <- lapply(plot$grobs$children, convert_gpath)
-
-  ## get the last grob
-  #last_grob <- grob_list[length(grob_list)]
-
-  ## remove the last grob from the list of grobs
-  #grob_list <- grob_list[- length(grob_list)]
-
-  ## remove all grobs except last one, not redrawing each time (to save time)
-  #invisible(lapply(grob_list, grid.remove, redraw = FALSE))
-
-  ## remove last grob with redrawing, now removing all the grobs
-  #invisible(lapply(last_grob, grid.remove))
-
   grid.remove(gPath(plot$grobs$name))
-
-  ## remove the viewport
-  #seekViewport(name = plot$grobs$vp$name)
-  #popViewport()
-
 
 }
 
