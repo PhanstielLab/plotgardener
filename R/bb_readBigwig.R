c49fe692863519b1059baca9d4c31b2d05f4582b
Nicole Kramer <nekramer@live.unc.edu>
2020-07-21 14:56:29 -0400

    null chromstart/chromend and multi plot signal
    Former-commit-id: 09e69b752d02d3f106ff2d1f9bb554f47d3d7b08

--- R/bb_readBigwig.R
+++ R/bb_readBigwig.R
@@ -5,23 +5,25 @@
 #' @param chromstart first base number to consider
 #' @param chromend last base number to consider
 #' @param strand strand, i.e. '+', '-', or '*'
+#'
+#' @return Function will return a 6-column dataframe of bigwig information.
 #' @export
 #'
 bb_readBigwig <- function(filename, chrom = NULL, chromstart = 1, chromend = .Machine$integer.max, strand = '*') {
 
   if (!hasArg(filename)) {
 
-    stop("Filename must be specified.")
+    stop("Filename must be specified.", call. = FALSE)
 
   }
   if (is.null(filename)) {
 
-    stop("Filename cannot be NULL.")
+    stop("Filename cannot be NULL.", call. = FALSE)
 
   }
   if (chromend < chromstart - 1) {
 
-    stop("End must be >= start - 1.")
+    stop("End must be >= start - 1.", call. = FALSE)
 
   }
 
--- R/bb_removeplot.R
+++ R/bb_removeplot.R
@@ -8,5 +8,15 @@ bb_removePlot <- function(plot){
   ## error function: need a plot that's a valid BentoBox plot, make sure the plot is plotted
   grid.remove(gPath(plot$grobs$name))
 
+
+  ## Need to remove outer viewport of bb_trianglehic plot
+  if (class(plot) == "bb_trianglehic"){
+    vp_name <- plot$grobs$vp$name
+    vp_name <- gsub("inside", "outside", vp_name)
+    seekViewport(vp_name)
+    popViewport()
+
+  }
+
 }
 
