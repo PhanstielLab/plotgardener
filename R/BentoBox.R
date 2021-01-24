dc89bee941324af5f3dfbc95f1e4275979e31fa0
Eric Davis <ericscottdavis@outlook.com>
2021-01-23 23:19:40 -0500

    Update DESCRIPTION, README, and improve pkgdown site

---
+++ R/BentoBox.R
@@ -0,0 +1,13 @@
+#' BentoBox: Coordinate-based Genomic Visualization Package for R
+#'
+#' BentoBox is a coordinate-based genomic visualization package for R. It grants users
+#' the ability to programmatically produce complex, multi-paneled figures. Tailored for
+#' genomics, BentoBox allows users to visualize large complex genomic datasets and
+#' provides exquisite control over how plots are placed and arranged on a page.
+#'
+#'
+#' @docType package
+#' @name BentoBox
+#' @useDynLib BentoBox
+#' @importFrom Rcpp sourceCpp
+"_PACKAGE"
