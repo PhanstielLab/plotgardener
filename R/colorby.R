85d1b752c13e961fae21d15636eb26ba161f7858
Nicole Kramer <nekramer@live.unc.edu>
2021-02-11 19:57:20 -0500

    plotPileup to plotBed and single line plotting
    Former-commit-id: 44d85c29d2d7b7ee3abc483a5f228a677394472e

--- R/colorby.R
+++ R/colorby.R
@@ -1,7 +1,7 @@
 #' Handle BentoBox color scaling parameters
 #'
 #' \code{colorby} should be used to create a set of parameters that specify color scaling for the functions \code{bb_plotBedpe},
-#' \code{bb_plotBedpeArches}, and \code{bb_plotPileup}.
+#' \code{bb_plotBedpeArches}, and \code{bb_plotBed}.
 #'
 #' @param column String specifying name of data column to scale colors by.
 #' @param range A numeric vector specifying the range of values to apply a color scale to.
