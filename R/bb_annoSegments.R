168b902e751211fd10269392f3d7e795b0b1c1e8
Nicole Kramer <nekramer@live.unc.edu>
2021-04-19 16:05:16 -0400

    bb_annoSegments

--- R/bb_annoSegments.R
+++ R/bb_annoSegments.R
@@ -1,12 +1,11 @@
-#' Draw a line segment within a BentoBox layout
+#' Annotates a line segment within a BentoBox plot
 #'
 #' @param x0 A numeric vector or unit object indicating the starting x-values of the line segments.
-#' @param y0 A numeric vector, unit object, or a character vector of values containing a "b" combined with a numeric value specifying starting y-values of the line segments.
-#' The character vector will place starting y-values relative to the bottom of the most recently plotted BentoBox plot according to the units of the BentoBox page.
+#' @param y0 A numeric vector or unit object indicating the starting y-values of the line segments.
 #' @param x1 A numeric vector or unit object indicating the stopping x-values of the line segments.
-#' @param y1 A numeric vector, unit object, or a character vector of values containing a "b" combined with a numeric value specifying stopping y-values of the line segments.
-#' The character vector will place stopping y-values relative to the bottom of the most recently plotted BentoBox plot according to the units of the BentoBox page.
-#' @param default.units A string indicating the default units to use if \code{x0}, \code{y0}, \code{x1}, or \code{y1} are only given as numeric vectors. Default value is \code{default.units = "inches"}.
+#' @param y1 A numeric vector or unit object indicating the stopping y-values of the line segments.
+#' @param plot Input BentoBox plot to internally plot line segments relative to.
+#' @param default.units A string indicating the default units to use if \code{x0}, \code{y0}, \code{x1}, or \code{y1} are only given as numeric vectors. Default value is \code{default.units = "native"}.
 #' @param linecolor A character value specifying segment line color. Default value is \code{linecolor = "black"}.
 #' @param lwd A numeric specifying segment line width. Default value is \code{lwd = 1}.
 #' @param lty A numeric specifying segment line type. Default value is \code{lty = 1}.
@@ -30,25 +29,36 @@
 #'
 #' @examples
 #' ## Create a BentoBox page
-#' bb_pageCreate(width = 7.5, height = 6, default.units = "inches")
+#' bb_pageCreate(width = 7.5, height = 2.5, default.units = "inches")
 #'
-#' ## Plot one line segment
-#' bb_plotSegments(x0 = 3.75, y0 = 0.25, x1 = 3.75, y1 = 5.75, default.units = "inches",
-#'                 lwd = 3, lty = 2)
+#' ## Plot a Manhattan plot
+#' library("TxDb.Hsapiens.UCSC.hg19.knownGene")
+#' data("bb_gwasData")
+#' manhattanPlot <- bb_plotManhattan(data = bb_gwasData, assembly = "hg19",
+#'                                   fill = c("grey", "#37a7db"), sigLine = TRUE,
+#'                                   col = "grey", lty = 2, range = c(0, 14),
+#'                                   x = 0.5, y = 0, width = 6.5, height = 2,
+#'                                   just = c("left", "top"), default.units = "inches")
 #'
-#' ## Plot multiple line segments at different locations in different colors
-#' bb_plotSegments(x0 = 0.5, y0 = c(1, 3, 5), x1 = 3.25, y1 = c(1, 3, 5),
-#'                 default.units = "inches",
-#'                 lwd = 2, linecolor = c("#7ecdbb", "#37a7db", "grey"))
+#'## Annotate genome label
+#' bb_annoGenomeLabel(plot = manhattanPlot, x = 0.5, y = 2, fontsize = 8, just = c("left", "top"),
+#'                    default.units = "inches" )
+#' bb_plotText(label = "Chromosome", fontsize = 8,
+#'             x = 3.75, y = 2.20, just = "center", default.units = "inches")
 #'
-#' ## Plot a line segment with an arrowhead
-#' bb_plotSegments(x0 = 4.5, y0 = 0.5, x1 = 7, y1 = 3, default.units = "inches",
-#'                 arrow = arrow(type = "closed"), fill = "black")
+#' ## Annotate y-axis
+#' bb_annoYaxis(plot = manhattanPlot, at = c(0, 2, 4, 6, 8, 10, 12, 14),
+#'              axisLine = TRUE, fontsize = 8)
 #'
-#' ## Plot lines with round lineends
-#' bb_plotSegments(x0 = c(4, 7), y0 = 3.5, x1 = 5.5, y1 = 4.5,
-#'                 default.units = "inches",
-#'                 lwd = 5, lineend = "round")
+#' ## Annotate a line segment for an additional significance line of the Manhattan plot
+#' bb_annoSegments(x0 = unit(0, "npc"), y0 = 10,
+#'                 x1 = unit(1, "npc"), y1 = 10,
+#'                 plot = manhattanPlot, default.units = "native",
+#'                 linecolor = "red", lty = 2)
+#'
+#' ## Plot y-axis label
+#' bb_plotText(label = "-log10(p-value)", x = 0.15, y = 1, rot = 90,
+#'             fontsize = 8, fontface = "bold", just = "center", default.units = "inches")
 #'
 #' ## Hide page guides
 #' bb_pageGuideHide()
@@ -56,8 +66,8 @@
 #' @seealso \link[grid]{grid.segments}, \link[grid]{arrow}
 #'
 #' @export
-bb_plotSegments <- function(x0, y0, x1, y1, default.units = "inches", linecolor = "black", lwd = 1, lty = 1, lineend = "butt", linejoin = "mitre", arrow = NULL, params = NULL, ...){
-
+bb_annoSegments <- function(x0, y0, x1, y1, plot, default.units = "native", linecolor = "black", lwd = 1, lty = 1,
+                            lineend = "butt", linejoin = "mitre", arrow = NULL, params = NULL, ...){
 
   # ======================================================================================================================================================================================
   # PARSE PARAMETERS
@@ -73,14 +83,15 @@ bb_plotSegments <- function(x0, y0, x1, y1, default.units = "inches", linecolor 
   if(missing(default.units)) default.units <- NULL
 
   ## Check if x0/y0/x1/y1 arguments are missing (could be in object)
+  if(!hasArg(plot)) plot <- NULL
   if(!hasArg(x0)) x0 <- NULL
   if(!hasArg(y0)) y0 <- NULL
   if(!hasArg(x1)) x1 <- NULL
   if(!hasArg(y1)) y1 <- NULL
 
   ## Compile all parameters into an internal object
-  bb_segmentsInternal <- structure(list(x0 = x0, y0 = y0, x1 = x1, y1 = y1, arrow = arrow, linecolor = linecolor,
-                                      lwd = lwd, lty = lty, lineend = lineend, linejoin = linejoin, default.units = default.units), class = "bb_segmentsInternal")
+  bb_segmentsInternal <- structure(list(plot = plot, x0 = x0, y0 = y0, x1 = x1, y1 = y1, arrow = arrow, linecolor = linecolor,
+                                        lwd = lwd, lty = lty, lineend = lineend, linejoin = linejoin, default.units = default.units), class = "bb_segmentsInternal")
 
   bb_segmentsInternal <- parseParams(bb_params = params, object_params = bb_segmentsInternal)
 
@@ -91,11 +102,11 @@ bb_plotSegments <- function(x0, y0, x1, y1, default.units = "inches", linecolor 
   if(is.null(bb_segmentsInternal$lty)) bb_segmentsInternal$lty <- 1
   if(is.null(bb_segmentsInternal$lineend)) bb_segmentsInternal$lineend <- "butt"
   if(is.null(bb_segmentsInternal$linejoin)) bb_segmentsInternal$linejoin <- "mitre"
-  if(is.null(bb_segmentsInternal$default.units)) bb_segmentsInternal$default.units <- "inches"
+  if(is.null(bb_segmentsInternal$default.units)) bb_segmentsInternal$default.units <- "native"
 
   ## Set gp
   bb_segmentsInternal$gp <- gpar(col = bb_segmentsInternal$linecolor, lwd = bb_segmentsInternal$lwd,
-                               lty = bb_segmentsInternal$lty, lineend = bb_segmentsInternal$lineend, linejoin = bb_segmentsInternal$linejoin)
+                                 lty = bb_segmentsInternal$lty, lineend = bb_segmentsInternal$lineend, linejoin = bb_segmentsInternal$linejoin)
   bb_segmentsInternal$gp <- setGP(gpList = bb_segmentsInternal$gp, params = bb_segmentsInternal, ...)
 
   # ======================================================================================================================================================================================
@@ -110,7 +121,8 @@ bb_plotSegments <- function(x0, y0, x1, y1, default.units = "inches", linecolor 
   # CATCH ERRORS
   # ======================================================================================================================================================================================
 
-  check_bbpage(error = "Cannot plot segment without a BentoBox page.")
+  check_bbpage(error = "Cannot annotate segment without a BentoBox page.")
+  if(is.null(bb_segmentsInternal$plot)) stop("argument \"plot\" is missing, with no default.", call. = FALSE)
   if(is.null(bb_segments$x0)) stop("argument \"x0\" is missing, with no default.", call. = FALSE)
   if(is.null(bb_segments$y0)) stop("argument \"y0\" is missing, with no default.", call. = FALSE)
   if(is.null(bb_segments$x1)) stop("argument \"x1\" is missing, with no default.", call. = FALSE)
@@ -146,16 +158,7 @@ bb_plotSegments <- function(x0, y0, x1, y1, default.units = "inches", linecolor 
 
     ## Check for "below" y0-coords
     if (all(grepl("b", bb_segments$y0)) == TRUE){
-      if (any(grepl("^[ac-zA-Z]+$", bb_segments$y0)) == TRUE){
-        stop("\'below\' y0-coordinate(s) detected with additional letters. Cannot parse y0-coordinate(s).", call. = FALSE)
-      }
-
-      if(any(is.na(as.numeric(gsub("b","", bb_segments$y0))))){
-        stop("\'below\' y0-coordinate(s) does not have a numeric associated with it. Cannot parse y0-coordinate(s).", call. = FALSE)
-      }
-
-      bb_segments$y0 <- unit(unlist(lapply(bb_segments$y0, plot_belowY)), get("page_units", envir = bbEnv))
-
+      stop("\'below\' y0-coordinate detected. Cannot parse \'below\' y-coordinate for bb_annoSegments.", call. = FALSE)
     } else {
 
       if (!is.numeric(bb_segments$y0)){
@@ -199,16 +202,7 @@ bb_plotSegments <- function(x0, y0, x1, y1, default.units = "inches", linecolor 
 
     ## Check for "below" y1-coords
     if (all(grepl("b", bb_segments$y1)) == TRUE){
-      if (any(grepl("^[ac-zA-Z]+$", bb_segments$y1)) == TRUE){
-        stop("\'below\' y1-coordinate(s) detected with additional letters. Cannot parse y1-coordinate(s).", call. = FALSE)
-      }
-
-      if(any(is.na(as.numeric(gsub("b","", bb_segments$y1))))){
-        stop("\'below\' y1-coordinate(s) does not have a numeric associated with it. Cannot parse y1-coordinate(s).", call. = FALSE)
-      }
-
-      bb_segments$y1 <- unit(unlist(lapply(bb_segments$y1, plot_belowY)), get("page_units", envir = bbEnv))
-
+      stop("\'below\' y1-coordinate detected. Cannot parse \'below\' y-coordinate for bb_annoSegments.", call. = FALSE)
     } else {
       if (!is.numeric(bb_segments$y1)){
 
@@ -228,18 +222,46 @@ bb_plotSegments <- function(x0, y0, x1, y1, default.units = "inches", linecolor 
 
 
   }
-  ## Convert coordinates to page_units
+
+
+  if (class(bb_segmentsInternal$plot) == "bb_genes"){
+
+    plotVP <- bb_segmentsInternal$plot$grobs$children$background$vp
+
+  } else if (class(bb_segmentsInternal$plot) == "bb_hicTriangle" | class(bb_segmentsInternal$plot) == "bb_hicRectangle"){
+
+    plotVP <- bb_segmentsInternal$plot$outsideVP
+
+  } else {
+
+    plotVP <- bb_segmentsInternal$plot$grobs$vp
+
+  }
+
+  ## Convert plot viewport to bottom left to get position on entire page
+  plotVP_bottomLeft <- vp_bottomLeft(viewport = plotVP)
+
+  ## Push plot viewport to convert x/y from plot native units to page units
+  seekViewport(plotVP$name)
   new_x0 <- convertX(bb_segments$x0, unitTo = page_units, valueOnly = TRUE)
-  new_y0 <- convertY(bb_segments$y0, unitTo = page_units, valueOnly = TRUE)
   new_x1 <- convertX(bb_segments$x1, unitTo = page_units, valueOnly = TRUE)
+  new_y0 <- convertY(bb_segments$y0, unitTo = page_units, valueOnly = TRUE)
   new_y1 <- convertY(bb_segments$y1, unitTo = page_units, valueOnly = TRUE)
+
+  seekViewport(name = "bb_page")
+
+  ## Add additional page units to new_x and new_y
+  new_x0 <- as.numeric(plotVP_bottomLeft[[1]]) + new_x0
+  new_x1 <- as.numeric(plotVP_bottomLeft[[1]]) + new_x1
+  new_y0 <- as.numeric(plotVP_bottomLeft[[2]]) + new_y0
+  new_y1 <- as.numeric(plotVP_bottomLeft[[2]]) + new_y1
 
   # ======================================================================================================================================================================================
   # MAKE GROB
   # ======================================================================================================================================================================================
 
-   segments <- grid.segments(x0 = unit(new_x0, page_units), y0 = unit(page_height - new_y0, page_units), x1 = unit(new_x1, page_units),
-                             y1 = unit(page_height - new_y1, page_units), arrow = bb_segments$arrow, gp = bb_segments$gp)
+  segments <- grid.segments(x0 = unit(new_x0, page_units), y0 = unit(new_y0, page_units), x1 = unit(new_x1, page_units),
+                            y1 = unit(new_y1, page_units), arrow = bb_segments$arrow, gp = bb_segments$gp)
 
   # ======================================================================================================================================================================================
   # ADD GROB TO OBJECT
