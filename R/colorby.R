3f1bfd6e0c6e90b496f56d68f9d0a2ff5acc803c
Nicole Kramer <nekramer@live.unc.edu>
2021-04-23 20:35:22 -0400

    R CMD check and BiocCheck updates
    Former-commit-id: 2305123da26d33b85870e79850420186429908df

--- R/colorby.R
+++ R/colorby.R
@@ -1,16 +1,19 @@
 #' Handle BentoBox color scaling parameters
 #'
-#' \code{colorby} should be used to create a set of parameters that specify color scaling for the functions \code{bb_plotBedpe},
+#' \code{colorby} should be used to create a set of parameters
+#' that specify color scaling for the functions \code{bb_plotBedpe},
 #' \code{bb_plotBedpeArches}, and \code{bb_plotBed}.
 #'
 #' @param column String specifying name of data column to scale colors by.
-#' @param range A numeric vector specifying the range of values to apply a color scale to.
+#' @param range A numeric vector specifying the range of values to
+#' apply a color scale to.
 #'
 #' @return Returns a "\code{bb_colorby}" object.
 #' @export
 colorby <- function(column, range = NULL){
 
-  colorbyObject <- structure(list(column = column, range = range), class = "bb_colorby")
+  colorbyObject <- structure(list(column = column, range = range),
+                             class = "bb_colorby")
   return(colorbyObject)
 
 }
