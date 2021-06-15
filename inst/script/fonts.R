fa19fd4cf20e0d8b88e9164b8692f8607ed9d277
Nicole Kramer <nekramer@live.unc.edu>
2021-06-15 10:25:47 -0400

    sources added to ggplot ex figure
    Former-commit-id: 35ecf4910a4c3c51d179fbfe176e154bf7c80a02 [formerly 5e215f78894886d48319dc81a148ab0f904cff07]
    Former-commit-id: bd7d46bea6520ee055e3699a160eb5a7a23a9a63
    Former-commit-id: 9b9331ef5d4a340d9a099f27691df5a754892b76
    Former-commit-id: e5e0a0aeab20e8f2a353737014a2e230556eabc2
    Former-commit-id: eead9e10ca21670cef352adfa87ddb69e501f7bf
    Former-commit-id: 3b301c5dc2fd92df52f66fd1a101cca4c7c43e5b
    Former-commit-id: 51fd40222330e3ed2e401d6d0af75f9888457e68
    Former-commit-id: e545518c01716fafb7ecce35801a4bf87b1db033
    Former-commit-id: 39f8c8d2df1e651e3e815657e9b7150fb94b9b29
    Former-commit-id: 209f89dd69cf78003813e045085c5b71336a06c6

--- inst/script/fonts.R
+++ inst/script/fonts.R
@@ -9,3 +9,8 @@ download.file(
 download.file(
     url = "https://www.download-free-fonts.com/details/92405/proxima-nova-semibold",
     destfile = "inst/extdata/proximanova-semibold.otf")
+
+## Regular italic
+download.file(
+    url = "https://www.download-free-fonts.com/details/93063/proxima-nova-regular-italic",
+    destfile = "inst/extdata/proximanova-regitalic.otf")
